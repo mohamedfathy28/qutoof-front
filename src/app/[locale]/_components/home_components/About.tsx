3cf9153c46c10c0a040f491ad25d09624ccfc918
Mohamed Fathy <55698746+mohamedfathy28@users.noreply.github.com>
2025-04-16 22:09:40 +0200

    0

--- src/app/[locale]/_components/home_components/About.tsx
+++ src/app/[locale]/_components/home_components/About.tsx
@@ -38,14 +38,14 @@ const About = () => {
         fetchData();
       }, []); // Empty dependency array ensures this runs only once after the component mounts
 
-      if (data?.title == '' && data?.content == '' ) return null
+      if (data?.title == '' && data?.content == ''  ) return null
 
 
   return (
     <div className={'py-20 md:py-32 grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto max-w-[90%] sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl'}>
     <div className=''>
       <Image data-aos="zoom-out" data-aos-duration="500" data-aos-delay="0"    width={500}
-             height={300}  src={data?.image || contactUs}  alt='contact us' />
+             height={300}  src={(data?.image)??contactUs}  alt='contact us' />
     </div>
     <div className=' flex flex-col gap-8'>
       <h6 data-aos="fade-left" data-aos-duration="500" data-aos-delay="0" className='text-[#009444] font-bold text-[16px] ltr:pl-4 ltr:border-l-[3px] rtl:pr-4 rtl:border-r-[3px] border-[#E5EDD3]'>{t("AboutQuttouf")}</h6>
